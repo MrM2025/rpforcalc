7ffc5de3c6cbb2c7b1acadee5faca1175634b4d5
Your Name <you@example.com>
2024-12-21 03:34:51 +0300

    filling the README.md

--- calc_go/cmd/main.go
+++ calc_go/cmd/main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/MrM2025/rpforcalc/tree/master/calc_go/pkg/calculation"
+	"github.com/MrM2025/rpforcalc/tree/master/calc_go/internal/application"
 )
 
 func main() {
- 
-	var calc calculation.TCalc
-
-	calc = calc.Init()
-	fmt.Println(calc.Calc("-1+1*2.54+41+((3/3+10)/2-(-2.5-1+(-1))*10)-1"))
-	fmt.Println(calc.Calc("1+1"))
-	fmt.Println(calc.Calc("(2+2)*2"))
-	fmt.Println(calc.Calc("2+2*2"))
-	fmt.Println(calc.Calc("1/2"))
-	fmt.Println(calc.Calc("1+1*"))
-	fmt.Println(calc.Calc(""))
-	fmt.Println(calc.Calc("1+((3/3+10)/2-2.5*10)"))
-	fmt.Println(calc.Calc("-1+2+(-3)"))
-	history := calc.GetCalcHistory()
-	for key, _ := range history {
-		fmt.Println(key, "result", history[key])
-	}
-	calc.RemoveHistory()
-	fmt.Println("123", calc.GetCalcHistory())
+	app := application.New()
+	// app.Run()
+	app.RunServer()
 }
