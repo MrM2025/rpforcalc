7489b30f7c627c5ae42984ec6a08689755fd0bab
Your Name <you@example.com>
2024-12-21 20:23:38 +0300

    WIN

--- calc_go/internal/application/application_test.go
+++ calc_go/internal/application/application_test.go
@@ -3,8 +3,6 @@ package application_test
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,35 +15,50 @@ import (
 type RequestBody struct {
 	Expression string `json:"expression"`
 }
+
 // верный запрос
 func TestCalcHandler_Success(t *testing.T) {
-	expected := "2"	
 
-	r := httptest.NewRequest(http.MethodGet, "/api/v1/calculate?expression=", nil)
-	w := httptest.NewRecorder()
-	application.CalcHandler(w, r)
+	handler := http.HandlerFunc(application.CalcHandler)
+	server := httptest.NewServer(handler)
+	defer server.Close()
 
-	res := w.Result()
-
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
+	// Запрос с правильным выражением
+	requestBody := RequestBody{
+		Expression: "1+1",
+	}
+	body, err := json.Marshal(requestBody)
 	if err != nil {
-		t.Errorf("Error: %v", err)
+		t.Fatalf("Error marshalling request body: %v", err)
 	}
 
-	if string(data) != expected {
-		fmt.Fprintf(w, "error")
+	// Создание POST-запроса
+	req, err := http.NewRequest("POST", server.URL+"/api/v1/calculate", bytes.NewBuffer(body))
+	if err != nil {
+		t.Fatalf("Error creating request: %v", err)
 	}
 
-	if w.Result().StatusCode != http.StatusOK {
-		fmt.Fprintf(w, "StatusCode error")
+	// Отправка запроса и получение ответа
+	resp, err := server.Client().Do(req)
+	if err != nil {
+		t.Fatalf("Error sending request: %v", err)
 	}
 
-	//defer r.Body.Close()
+	// проверка, что статус ответа 200
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status 200 OK, got %d", resp.StatusCode)
+	}
 
-
-	
+	// Проверка ответа
+	var response application.CalcResJSON
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		t.Fatalf("Error decoding response: %v", err)
+	}
+	expectedResult := "2.000000"
+	if response.Result != expectedResult {
+		t.Fatalf("Expected result %s, got %s", expectedResult, response.Result)
+	}
 }
 
 // неверное выражение
@@ -91,51 +104,6 @@ func TestCalcHandler_InvalidExpression(t *testing.T) {
 	}
 	if response.Error != calculation.IncorrectExpressionErr.Error() {
 		t.Fatalf("Expected error %v, got %v", calculation.IncorrectExpressionErr, response.Error)
-	}
-}
-
-// Тест ошибки деления на ноль
-func TestCalcHandler_DivisionByZero(t *testing.T) {
-	handler := http.HandlerFunc(application.CalcHandler)
-	server := httptest.NewServer(handler)
-	defer server.Close()
-
-	// запрос с делением на ноль
-	requestBody := RequestBody{
-		Expression: "1/0",
-	}
-	body, err := json.Marshal(requestBody)
-	if err != nil {
-		t.Fatalf("Error marshalling request body: %v", err)
-	}
-
-	// Создание POST-запроса
-	req, err := http.NewRequest("POST", server.URL+"/api/v1/calculate", bytes.NewBuffer(body))
-	if err != nil {
-		t.Fatalf("Error creating request: %v", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	// отправка запроса и получение ответа
-	resp, err := server.Client().Do(req)
-	if err != nil {
-		t.Fatalf("Error sending request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// проверка, что статус ответа 422
-	if resp.StatusCode != http.StatusUnprocessableEntity {
-		t.Fatalf("Expected status 422, got %d", resp.StatusCode)
-	}
-
-	// Проверка ответа
-	var response application.CalcResJSON
-	err = json.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		t.Fatalf("Error decoding response: %v", err)
-	}
-	if response.Error != calculation.DvsByZeroErr.Error() {
-		t.Fatalf("Expected error %v, got %v", calculation.DvsByZeroErr, response.Error)
 	}
 }
 
